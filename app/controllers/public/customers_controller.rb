a3cf0b1de1da867aa122166207629fdb51ff9ac8
EC2 Default User <ec2-user@ip-172-31-3-147.ap-northeast-1.compute.internal>
2023-08-26 08:05:28 +0000

    20230826_顧客用：新規登録画面実装中

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,13 @@
+class Public::CustomersController < ApplicationController
+  def show
+    @customer = current_customer
+  end
+
+  def edit
+    @customer = current_customer
+  end
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
+  root to: 'public/homes#top'
+  get 'home/about' => 'public/homes#about', as: 'about'
+  namespace :customers do
+    resources :customers,only: [:show, :edit, :update]
+    get 'mypage' => 'public/customers#show'
   end
+
   namespace :admin do
-  resources :genres,only: [:index,:create,:edit,:update]
-  resources :items,only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres,only: [:index,:create,:edit,:update]
+    resources :items,only: [:index, :new, :create, :show, :edit, :update]
   end
+
 # 顧客用
 # URL /customers/sign_in ...
   devise_for :customers, skip: [:passwords], controllers: {
