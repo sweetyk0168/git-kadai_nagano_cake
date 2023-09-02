d575500d865f89ee98a02f9c6d4ceadc91d60293
EC2 Default User <ec2-user@ip-172-31-3-147.ap-northeast-1.compute.internal>
2023-09-02 09:03:59 +0000

    20230902_管理者用：商品一覧画面実装中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,30 @@
 class Admin::ItemsController < ApplicationController
+  layout 'admin_header'
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path(@item)
+    else
+      render "new"
+    end
+  end
+  
   def show
+    
   end
 
   def edit
+  end
+  
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active, :image)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :introduction, presence:true
+  validates :price, presence:true
+  enum is_active: {販売中:0,販売停止:1}
+  has_one_attached :image
+  validates :image, presence: true
 end
