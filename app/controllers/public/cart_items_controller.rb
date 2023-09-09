5926cfb14e76011d811c43964fef07aedc0b2a64
EC2 Default User <ec2-user@ip-172-31-3-147.ap-northeast-1.compute.internal>
2023-09-09 04:55:50 +0000

    20230909_会員用：カート内編集画面実装中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,6 +1,6 @@
 class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
-  before_action :cart_item_item?, only: [:create]
+
   
   def index
     @cart_items = current_customer.cart_items
@@ -8,54 +8,21 @@ class Public::CartItemsController < ApplicationController
   end
   
   def create
-    #カート内に商品があるか？
-    if current_customer.cart_items.count >= 1
-      #カートに入れた商品はすでにカートに追加済か？
-      if nil != current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-        #カート内のすでにある商品の情報取得
-        @cart_item_u = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-        #既にある情報に個数を合算
-        @cart_item_u.number_of_items += params[:cart_item][:number_of_items].to_i
-        #情報の更新 個数カラム
-        @cart_item_u.update(number_of_items: @cart_item_u.number_of_items)
-        #カートページ遷移
-        redirect_to_cart_items_path
-      else
-        #新しいカートの作成
-        @cart_item = CartItem.new(cart_item_params)
+  #カート内に同一商品があるか？
+    @in_cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    if @in_cart_item
+       @sum_amount = @in_cart_item.number_of_items + params[:cart_item][:number_of_items].to_i
+       @in_cart_item.update(number_of_items: @sum_amount)
+       redirect_to cart_items_path
+    else
+      #新しいカートの作成
+      @cart_item = CartItem.new(cart_item_params)
         #誰のカートか紐づけ
         @cart_item.customer_id = current_customer.id
         #情報を保存できるか？
-        if @cart_item.save
-          #カートページ遷移
-          redirect_to cart_items_path
-        else
-          #販売ステータスが「０」のものを見つける
-          @item = Item.where(sale_status: 0).page(params[:page]).per(8)
-          #商品の数をカウント
-          @amount = Item.count
-          #有効、無効ステータスが0のものを見つける
-          @genres = Genre.where(is_deleted: 0)
-          render 'index'
-        end
-      end
-    else
-      #新しくカートの作成
-      @cart_item = CartItem.new(cart_item_params)
-      #誰のカートか紐づけ
-      @cart_item.customer_id = current_customer.id
-      #情報を保存できるか？
-      if @cart_item.save
+        @cart_item.save
         #カートページ遷移
         redirect_to cart_items_path
-      else
-        @items = Item.where(is_active: 0).page(params[:page]).per(8)
-        #商品の数をカウント
-        @amount = Item.count
-        #有効、無効ステータスが0のものを見つける
-        @genres = Genre.where(is_deleted: 0)
-        render 'index'
-      end
     end
   end
 
@@ -82,9 +49,6 @@ class Public::CartItemsController < ApplicationController
   
   private
   
-  def cart_item_item?
-    redirect_to cart_item_path(params[:cart_item][:item_id]), notice: "個数を入力してください。"if params[:cart_item][:number_of_items].empty?
-  end
   
   def cart_item_params
     params.require(:cart_item).permit(:amount, :item_id)
